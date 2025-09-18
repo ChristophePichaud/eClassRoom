6c7909ef8516245d5ab456c497f3a5acc3b420a7
Christophe Pichaud <christophepichaud@hotmail.com>
2025-09-18 19:59:43 +0200

    Clients page updated...

--- Client/Pages/Clients.razor.cs
+++ Client/Pages/Clients.razor.cs
@@ -7,38 +7,90 @@ using Shared.Dtos;
 
 namespace Client.Pages
 {
-    public partial class Clients
+    public class ClientsBase : ComponentBase
     {
-        [Inject] private HttpClient Http { get; set; }
+        [Inject] protected HttpClient Http { get; set; }
 
-        private List<ClientDto> clients;
-        private bool showForm = false;
-        private ClientDto newClient = new();
+        protected List<ClientDto> clients;
+        protected bool isLoading = true;
+        protected bool showForm = false;
+        protected bool isEdit = false;
+        protected ClientDto editClient = new();
 
         protected override async Task OnInitializedAsync()
         {
             await LoadClientsAsync();
         }
 
-        private async Task LoadClientsAsync()
+        protected async Task LoadClientsAsync()
         {
+            isLoading = true;
             clients = await Http.GetFromJsonAsync<List<ClientDto>>("api/clients");
+            isLoading = false;
         }
 
-        private void ToggleForm()
+        protected void ShowAddClient()
         {
-            showForm = !showForm;
-            if (showForm)
-                newClient = new ClientDto();
+            editClient = new ClientDto();
+            showForm = true;
+            isEdit = false;
         }
 
-        private async Task CreateClientAsync()
+        protected void EditClient(ClientDto client)
         {
-            var response = await Http.PostAsJsonAsync("api/clients", newClient);
+            // Clone pour éviter la modification directe dans la liste
+            editClient = new ClientDto
+            {
+                Id = client.Id,
+                NomSociete = client.NomSociete,
+                Adresse = client.Adresse,
+                CodePostal = client.CodePostal,
+                Ville = client.Ville,
+                Pays = client.Pays,
+                EmailAdministrateur = client.EmailAdministrateur,
+                Mobile = client.Mobile,
+                MotDePasseAdministrateur = "" // Ne jamais pré-remplir le mot de passe
+            };
+            showForm = true;
+            isEdit = true;
+        }
+
+        protected void CancelEdit()
+        {
+            showForm = false;
+            isEdit = false;
+        }
+
+        protected async Task SaveClient()
+        {
+            if (isEdit)
+            {
+                var response = await Http.PutAsJsonAsync($"api/clients/{editClient.Id}", editClient);
+                if (response.IsSuccessStatusCode)
+                {
+                    await LoadClientsAsync();
+                    showForm = false;
+                }
+                // Gérer les erreurs si besoin
+            }
+            else
+            {
+                var response = await Http.PostAsJsonAsync("api/clients", editClient);
+                if (response.IsSuccessStatusCode)
+                {
+                    await LoadClientsAsync();
+                    showForm = false;
+                }
+                // Gérer les erreurs si besoin
+            }
+        }
+
+        protected async Task DeleteClient(int id)
+        {
+            var response = await Http.DeleteAsync($"api/clients/{id}");
             if (response.IsSuccessStatusCode)
             {
                 await LoadClientsAsync();
-                showForm = false;
             }
             // Gérer les erreurs si besoin
         }
